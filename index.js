1872f4f2ef40506f08e41de67897e33774922868
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2022-03-25 22:16:35 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const drivers = ["Jonny", "Allie", "D", "I", "Z", "G", "O"];
+
+const returnFirstTwoDrivers = function (drivers) {
+  return [drivers[0], drivers[1]];
+};
+
+const returnLastTwoDrivers = function (drivers) {
+  return [drivers[drivers.length - 2], drivers[drivers.length - 1]];
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int) {
+  return function (fare) {
+    return fare * int;
+  };
+}
+
+function fareDoubler(fare) {
+  return fare * 2;
+}
+
+function fareTripler(fare) {
+  return fare * 3;
+}
+
+function selectDifferentDrivers(arrayOfDrivers, f) {
+  return f(arrayOfDrivers);
+}
